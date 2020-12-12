f551165d0364c1803ba31de6f227f671a5bdf5df
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2020-12-12 23:17:05 +0100

    gitignore update

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -2,4 +2,3 @@ resource "aws_instance" "example" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
 }
-
--- demo-1/provider.tf
+++ demo-1/provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-  region     = var.AWS_REGION
+  profile = var.AWS_PROFILE
+  region  = var.AWS_REGION
 }
 
