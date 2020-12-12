fdcef7b3f161c1e8cd4e1eef5a59fcc29219326e
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2020-12-12 23:16:22 +0100

    first steps

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,11 +1,10 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  profile    = "personal"
+  region     = "eu-west-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-055958ae2f796344b"
   instance_type = "t2.micro"
 }
 
