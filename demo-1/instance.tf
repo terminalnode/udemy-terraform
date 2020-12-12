291867ae4111a30797d00071e1f8904e7c1d8e08
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2020-12-12 23:17:34 +0100

    output id/dns demo-1

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -2,3 +2,11 @@ resource "aws_instance" "example" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
 }
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
+
+output "public_dns" {
+  value = aws_instance.example.public_dns
+}
